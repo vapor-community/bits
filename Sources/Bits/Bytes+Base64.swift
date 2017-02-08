b735419c54b1f82590e246bf7607d3161ffafaa7
Logan Wright <logan.william.wright@gmail.com>
2017-02-08 14:01:05 +0100

    update base64 encoded to reflect changes to core

--- Sources/Bits/Bytes+Base64.swift
+++ Sources/Bits/Bytes+Base64.swift
@@ -1,42 +1,20 @@
 import Foundation
 
 extension Sequence where Iterator.Element == Byte {
-    /**
-        Converts a sequence of bytes into a base64 string representation
-    */
-    public var base64String: String {
-        return data.base64EncodedString()
-    }
+    public var base64Encoded: Bytes {
+        let bytes = [Byte](self)
+        let data = Data(bytes: bytes)
 
-    /**
-        Converts a sequence of bytes into a byte array complient w/ base64 encoding
-    */
-    public var base64Data: Bytes {
         let encodedData = data.base64EncodedData()
         return encodedData.makeBytes()
     }
 
-    private var data: Data {
+    public var base64Decoded: Bytes {
         let bytes = [Byte](self)
-        return Data(bytes: bytes)
-    }
-}
-
-extension String {
-    /**
-        Converts a base64 string to its original representation
-    */
-    public var base64DecodedString: String {
-        return base64DecodedData.string
-    }
-
-    /**
-        Decodes a base64 string to its original data representation
-    */
-    public var base64DecodedData: Bytes {
-        guard let data = NSData(base64Encoded: self, options: []) else { return [] }
-        var bytes = Bytes(repeating: 0, count: data.length)
-        data.getBytes(&bytes,  length: data.length)
-        return bytes
+        let dataBase64 = Data(bytes: bytes)
+        guard let dataDecoded = Data(base64Encoded: dataBase64) else {
+            return []
+        }
+        return dataDecoded.makeBytes()
     }
 }
