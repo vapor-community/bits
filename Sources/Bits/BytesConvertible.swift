90c8cc36c41f35bf49343bdfd42806d72ff00e57
Logan Wright <logan.william.wright@gmail.com>
2017-02-10 15:58:36 +0100

    public

--- Sources/Bits/BytesConvertible.swift
+++ Sources/Bits/BytesConvertible.swift
@@ -18,7 +18,7 @@ public protocol BytesInitializable {
 public protocol BytesConvertible: BytesRepresentable, BytesInitializable { }
 
 extension BytesInitializable {
-    init(bytes: BytesConvertible) throws {
+    public init(bytes: BytesConvertible) throws {
         let bytes = try bytes.makeBytes()
         try self.init(bytes: bytes)
     }
